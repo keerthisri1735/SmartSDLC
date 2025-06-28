22c88fd18b9370865749edad4998c65eefa62874
keerthisri1735 <158995227+keerthisri1735@users.noreply.github.com>
2025-06-28 13:55:31 +0530

    Delete pdf_utils.py

--- pdf_utils.py
+++
@@ -1,9 +0,0 @@
-import fitz  # PyMuPDF
-import io
-
-def extract_text_from_pdf(file_bytes):
-    text = ""
-    with fitz.open(stream=file_bytes, filetype="pdf") as doc:
-        for page in doc:
-            text += page.get_text()
-    return text
